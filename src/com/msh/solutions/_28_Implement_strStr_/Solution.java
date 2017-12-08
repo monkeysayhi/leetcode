b91844753a9a0ddbe5b460c13683b44f8d21480b
monkeysayhi <monkeysayhi@163.com>
2017-12-08 10:16:51 +0800

    Solved 28. Implement strStr()

---
+++ src/com/msh/solutions/_28_Implement_strStr_/Solution.java
@@ -0,0 +1,34 @@
+package com.msh.solutions._28_Implement_strStr_;
+
+/**
+ * Created by monkeysayhi on 2017/12/8.
+ */
+class Solution {
+  public int strStr(String haystack, String needle) {
+    if (haystack == null || needle == null) {
+      return -1;
+    }
+    if (haystack.length() < needle.length()) {
+      return -1;
+    }
+    if (needle.length() == 0) {
+      return 0;
+    }
+
+    int index = -1;
+    for (int i = 0; i + needle.length() <= haystack.length(); i++) {
+      boolean eq = true;
+      for (int j = 0; j < needle.length(); j++) {
+        if (haystack.charAt(i+j) != needle.charAt(j)) {
+          eq = false;
+          break;
+        }
+      }
+      if (eq) {
+        index = i;
+        break;
+      }
+    }
+    return index;
+  }
+}
