13b84b8c0812a5855cc19e71a95898e25c8b06d7
monkeysayhi <monkeysayhi@163.com>
2017-12-11 12:25:24 +0800

    Add comment

--- src/com/msh/solutions/_28_Implement_strStr_/Solution.java
+++ src/com/msh/solutions/_28_Implement_strStr_/Solution.java
@@ -4,6 +4,7 @@ package com.msh.solutions._28_Implement_strStr_;
  * Created by monkeysayhi on 2017/12/8.
  */
 class Solution {
+  // 暴力, O(m * n)
   public int strStr(String haystack, String needle) {
     if (haystack == null || needle == null) {
       return -1;
