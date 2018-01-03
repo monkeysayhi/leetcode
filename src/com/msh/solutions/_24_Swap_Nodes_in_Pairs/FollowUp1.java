7c9b34177074a0990d592432e311bbaa873f903d
monkeysayhi <monkeysayhi@163.com>
2018-01-03 13:49:07 +0800

    Add follow up 1 on 24. Swap Nodes in Pairs

---
+++ src/com/msh/solutions/_24_Swap_Nodes_in_Pairs/FollowUp1.java
@@ -0,0 +1,14 @@
+package com.msh.solutions._24_Swap_Nodes_in_Pairs;
+
+/**
+ * Created by monkeysayhi on 2018/1/3.
+ */
+// 按照长度k为组，反转组内节点
+public class FollowUp1 {
+  public ListNode swapPairs(ListNode head) {
+    // 类型不同
+    // return new com.msh.solutions._25_Reverse_Nodes_in_k_Group.Solution()
+    //     .reverseKGroup(head, 2);
+    return null;
+  }
+}
