b9830ad3ddbc433860e5b8b49bbe86c9218f52e3
monkeysayhi <monkeysayhi@163.com>
2018-03-15 22:19:57 +0800

    Modify comment

--- src/com/msh/solutions/_11_Container_With_Most_Water/Solution.java
+++ src/com/msh/solutions/_11_Container_With_Most_Water/Solution.java
@@ -4,10 +4,10 @@ package com.msh.solutions._11_Container_With_Most_Water;
  * Created by monkeysayhi on 2018/1/25.
  */
 public class Solution {
-  // 求max{(j - i) * min{height[i], height[j]}}
+  // 求max{|j - i| * min{height[i], height[j]}}
   // 暴力，n(n^2)，奔着O(n)或O(nlogn)的方向想
-  // 使其中一个因子保持递减或递增，保证另一个因子都扫描过即可，很自然想尝试双指针两头扫描
-  // 关注短板:
+  // 使其中一个因子保持递减或递增，保证另一个因子都扫描过即可。很自然想尝试双指针两头扫描，使|j - i|递减，那么只要扫描过(ansL, ansR)即可
+  // 扫描过程如下：
   // 1. 如果形如[l...ansL...ansR...r]，则必然满足 min{l, r} < min{ansL, ansR}，则一定不取min{l, r}，假设最小是l，则l++
   // 2. 如果形如[ansL...l...r...ansR]，则之前一定发生过形如[l...ansL...ansR...r]的情况，必然已经扫描得到过最大值
   // 注意，如果height[l] == height[r]，则移动两边均可
