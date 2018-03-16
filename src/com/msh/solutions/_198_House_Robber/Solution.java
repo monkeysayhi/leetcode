6c674206cff9ab3c952bbc9fb516ca41237237b5
monkeysayhi <monkeysayhi@163.com>
2018-03-16 22:05:56 +0800

    Remove useless sub state

--- src/com/msh/solutions/_198_House_Robber/Solution.java
+++ src/com/msh/solutions/_198_House_Robber/Solution.java
@@ -5,7 +5,9 @@ package com.msh.solutions._198_House_Robber;
  */
 public class Solution {
   // 很容易陷入求sum{nums[i], sum[i + 1]}的陷阱。。。
-  // 最优化，求最大，dp[i]为截止到抢劫第i家后能抢到的最大钱数，dp[i] = max{dp[i - j] + nums[i] | 2 <= j <= i}
+  // 最优化，求最大，dp[i]为顺序抢劫第i家后能抢到的最大钱数，dp[i] = nums[i] + max{dp[i - 2], dp[i - 3]}
+  // 或者设rob[i]为顺序抢劫第i家后能抢到的最大钱数，nrob[i]为顺序不抢劫第i家后能抢到的最大钱数
+  // 则 rob[i] = nrob[i - 1] + nums[i], nrob[i] = max{nrob[i - 1], rob[i - 1]}
   public int rob(int[] nums) {
     if (nums == null || nums.length == 0) {
       return 0;
@@ -18,15 +20,13 @@ public class Solution {
     }
 
     int n = nums.length;
-    int[] dp = new int[1 + n];
-    dp[0] = 0; // 之前一家都没抢
-    dp[1] = nums[0];
-    dp[2] = Math.max(nums[0], nums[1]);
+    int[] dp = new int[n];
+    dp[0] = nums[0];
+    dp[1] = nums[1];
+    dp[2] = nums[0] + nums[2];
     int max = Math.max(dp[1], dp[2]);
-    for (int i = 3; i <= n; i++) {
-      for (int j = 2; j <= i; j++) {
-        dp[i] = Math.max(dp[i], dp[i - j] + nums[i - 1]);
-      }
+    for (int i = 3; i < n; i++) {
+      dp[i] = nums[i] + Math.max(dp[i - 2], dp[i - 3]);
       max = Math.max(max, dp[i]);
     }
     return max;
--- src/com/msh/solutions/_213_House_Robber_2/Solution.java
+++ src/com/msh/solutions/_213_House_Robber_2/Solution.java
@@ -4,8 +4,11 @@ package com.msh.solutions._213_House_Robber_2;
  * Created by monkeysayhi on 2018/1/26.
  */
 public class Solution {
-  // 最优化，求最大，dp[i]为截止到抢劫第i家后能抢到的最大钱数，dp[i] = max{dp[i - j] + nums[i] | 2 <= j <= i}；
-  // 扫一遍nums[1...n-1]，一遍nums[2...n]，取最大值。这样既包括了所有情况，时间复杂度也没有增加
+  // 最优化，求最大，dp[i]为截止到抢劫第i家后能抢到的最大钱数，dp[i] = nums[i] + max{dp[i - 2], dp[i - 3]}
+  // 扫一遍nums[0...n-3]，nums[0...n-2]，一遍nums[1...n-1]，取最大值。这样既包括了所有情况，时间复杂度也没有增加
+  // 或者设rob[i]为顺序抢劫第i家后能抢到的最大钱数，nrob[i]为顺序不抢劫第i家后能抢到的最大钱数
+  // 则 rob[i] = nrob[i - 1] + nums[i], nrob[i] = max{nrob[i - 1], rob[i - 1]}
+  // 扫描逻辑相同
   public int rob(int[] nums) {
     if (nums == null || nums.length == 0) {
       return 0;
@@ -21,21 +24,27 @@ public class Solution {
     }
 
     int n = nums.length;
-    return Math.max(robInLine(nums, 0, n - 1),
-        robInLine(nums, 1, n - 1));
+    int max = robInLine(nums, 0, n - 2);
+    max = Math.max(max, robInLine(nums, 0, n - 1));
+    max = Math.max(max, robInLine(nums, 1, n - 1));
+    return max;
   }
 
   private int robInLine(int[] nums, int offset, int len) {
-    int[] dp = new int[1 + offset + len];
-    dp[offset] = 0;
-    dp[offset + 1] = nums[offset];
-    dp[offset + 2] = Math.max(nums[offset], nums[offset + 1]);
-    int max = Math.max(dp[offset + 1], dp[offset + 2]);
-    for (int i = 3; i <= len; i++) {
-      for (int j = 2; j <= i; j++) {
-        dp[offset + i] = Math.max(dp[offset + i], dp[offset + i - j] + nums[offset + i - 1]);
-      }
-      max = Math.max(max, dp[offset + i]);
+    if (len == 1) {
+      return nums[offset];
+    }
+    if (len == 2) {
+      return Math.max(nums[offset], nums[offset + 1]);
+    }
+    int[] dp = new int[len];
+    dp[0] = nums[offset];
+    dp[1] = nums[offset + 1];
+    dp[2] = nums[offset] + nums[offset + 2];
+    int max = Math.max(dp[1], dp[2]);
+    for (int i = 3; i < len; i++) {
+      dp[i] = nums[offset + i] + Math.max(dp[i - 2], dp[i - 3]);
+      max = Math.max(max, dp[i]);
     }
     return max;
   }
