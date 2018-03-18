cf5dc0b4505cca8e5a03e1701af463177e10cbae
monkeysayhi <monkeysayhi@163.com>
2018-03-18 13:30:17 +0800

    Opt method in comment

--- src/com/msh/solutions/_41_First_Missing_Positive/Solution.java
+++ src/com/msh/solutions/_41_First_Missing_Positive/Solution.java
@@ -4,13 +4,14 @@ package com.msh.solutions._41_First_Missing_Positive;
  * Created by monkeysayhi on 2018/2/4.
  */
 public class Solution {
-  // 没有完全想出来，看的两篇题解理解了，http://blog.csdn.net/liuchonge/article/details/73199068，http://bangbingsyb.blogspot.com/2014/11/leetcode-first-missing-positive.html
+  // solution 1: 没有完全想出来，看的两篇题解理解了，http://blog.csdn.net/liuchonge/article/details/73199068，http://bangbingsyb.blogspot.com/2014/11/leetcode-first-missing-positive.html
   // 依旧是如何原地保存中间状态的问题：
   // 1. 负数和0：不需要记录
   // 2. 正数：将 nums[i] 放到 nums[i] - 1 位置上（从0开始）
   // 最后遍历一遍，找到第一个不满足 `nums[i] == i + 1` 的元素，返回 i + 1
-  // 对于重复的正数元素，也不需要记录，可当做负数与0处理
-  // 如果存在大于nums.length的元素，则ans一定满足 `ans <= nums.length`，否则数组长度将大于nums.length。因此，大于nums.length的元素也不需要记录
+  // 对于重复的正数元素，也不需要记录，可忽略
+  // 如果存在大于nums.length的元素，则ans一定满足`ans <= nums.length`，否则数组长度将大于nums.length。因此，大于nums.length的元素也不需要记录
+  // 否则最坏情况下数据存在1~nums.length个元素，则`ans = nums.length + 1`
   public int firstMissingPositive(int[] nums) {
     // assume valid
     for (int i = 0; i < nums.length; i++) {
@@ -32,7 +33,7 @@ public class Solution {
     nums[j] = tmp;
   }
 
-  // // 非常量空间：set记录，从1开始遍历，检查缺失者
+  // // solution -1: 非常量空间：set记录全部正数元素，从1开始（单独检查），遍历所有e，检查e + 1是否存在
   // public int firstMissingPositive(int[] nums) {
   //     // assume valid
   //     Set<Integer> set = new HashSet<>();
@@ -41,10 +42,19 @@ public class Solution {
   //             set.add(num);
   //         }
   //     }
-  //     int ans = 1;
-  //     while (set.contains(ans)) {
-  //         ans++;
+  //     if (!set.contains(1)) {
+  //         return 1;
   //     }
-  //     return ans;
+  //     int minMissingPnum = Integer.MAX_VALUE;
+  //     for (Integer pnum : set) {
+  //         // 一定满足 minMissingPnum < Integer.MAX_VALUE，所以可忽略该case
+  //         if (pnum == Integer.MAX_VALUE) {
+  //             continue;
+  //         }
+  //         if (pnum + 1 < minMissingPnum && !set.contains(pnum + 1)) {
+  //             minMissingPnum = pnum + 1;
+  //         }
+  //     }
+  //     return minMissingPnum;
   // }
 }
