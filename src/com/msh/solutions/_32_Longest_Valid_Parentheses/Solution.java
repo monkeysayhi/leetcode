02172ceb171455b389dd02f4c6d7df1cd6871358
monkeysayhi <monkeysayhi@163.com>
2018-09-07 10:17:13 +0800

    Modify

--- src/com/msh/solutions/_32_Longest_Valid_Parentheses/Solution.java
+++ src/com/msh/solutions/_32_Longest_Valid_Parentheses/Solution.java
@@ -4,37 +4,34 @@ package com.msh.solutions._32_Longest_Valid_Parentheses;
  * Created by monkeysayhi on 2018/1/29.
  */
 public class Solution {
-  // 最优化，求最长，dp[i]为以位置i结尾的最长子串长度，dp[i] = max{dp[i - 1 - dp[i - 1] - 1](dp[i - 1]), dp[i - 2]()}
+  // solution 1: 最优化，求最长，设 dp[i] 为 “以位置i结尾的最长括号串的长度”，则 dp[i] =
+  // if s[i] == '(': 0
+  // else if s[i - 1] == '(': 2 + dp[i - 2]
+  // else if s[i - 1] == ')' && dp[i - 1] > 0 && s[i - 1 - dp[i - 1]] == '(': 2 + dp[i - 1] + dp[i - (2 + dp[i - 1])]
   // AC：O(n)，因此，先想时间复杂度低的，，，不行再想高的
   public int longestValidParentheses(String s) {
-    if (s == null || s.length() == 0) {
+    if (s == null || s.length() < 2) {
       return 0;
-    }
-    if (s.length() == 1) {
-      return 0;
-    }
-    if (s.length() == 2 && s.equals("()")) {
-      return 2;
     }
 
     char[] chars = s.toCharArray();
     int n = chars.length;
     int[] dp = new int[n];
     dp[0] = 0;
-    dp[1] = chars[0] == '(' && chars[1] == ')' ? 2 : 0;
-    int maxLen = Math.max(dp[0], dp[1]);
-    for (int i = 2; i < n; i++) {
-      dp[i] = 0;
-      if (chars[i] == ')') {
-        if (chars[i - 1] == ')' && dp[i - 1] > 0
-            && i - 1 >= dp[i - 1] && chars[i - 1 - dp[i - 1]] == '(') {
-          dp[i] = Math.max(dp[i], dp[i - 1] + 2);
-          if (i - 2 >= dp[i - 1] && dp[i - 2 - dp[i - 1]] > 0) {
-            dp[i] = Math.max(dp[i], dp[i - 2 - dp[i - 1]] + dp[i - 1] + 2);
-          }
+    int maxLen = 0;
+    for (int i = 1; i < n; i++) {
+      if (chars[i] == '(') {
+        dp[i] = 0;
+      } else if (chars[i - 1] == '(') {
+        dp[i] = 2;
+        if (i - 2 >= 0 && dp[i - 2] > 0) {
+          dp[i] += dp[i - 2];
         }
-        if (chars[i - 1] == '(') {
-          dp[i] = Math.max(dp[i], dp[i - 2] + 2);
+      } else if (chars[i - 1] == ')'
+          && dp[i - 1] > 0 && i - 1 - dp[i - 1] >= 0 && chars[i - 1 - dp[i - 1]] == '(') {
+        dp[i] = 2 + dp[i - 1];
+        if (i - (2 + dp[i - 1]) >= 0) {
+          dp[i] += dp[i - (2 + dp[i - 1])];
         }
       }
       maxLen = Math.max(maxLen, dp[i]);
@@ -42,7 +39,7 @@ public class Solution {
     return maxLen;
   }
 
-//     // 最优化，求最长，dp[i][j]为区间[i, j]是否是有效小括号串
+//     // solution -1: 最优化，求最长，dp[i][j]为区间[i, j]是否是有效小括号串
 //     // dp[i][j] = or{dp[i + 1][j - 1] && match(chars[i], chars[j]), dp[i][k] && dp[k + 1][j] | i < k < j - 1}
 //     // TLE：测试用例有1500长度的字符串，O(n^3)超时。想其他办法。
 //     public int longestValidParentheses(String s) {
