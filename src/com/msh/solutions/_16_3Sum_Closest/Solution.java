584626963d99a3c16da9b4e00688b6295725bac5
monkeysayhi <monkeysayhi@163.com>
2018-09-10 12:21:13 +0800

    Modify

--- src/com/msh/solutions/_16_3Sum_Closest/Solution.java
+++ src/com/msh/solutions/_16_3Sum_Closest/Solution.java
@@ -6,44 +6,43 @@ import java.util.Arrays;
  * Created by monkeysayhi on 2018/3/24.
  */
 public class Solution {
-  // solution 1: 暴力，枚举`<i, j, k>`。O(n^3)
-  // solution 2: 排序，对每个数，求“2Sum Closest”。O(n^2)
+  // solution 1: 暴力，枚举<i, j, k>，维护 cloest tuple，O(n^3)
+  // solution 2: 排序，枚举 nums[k]，带着nums[k]两头双指针维护 cloest tuple，O(n^2)
   public int threeSumClosest(int[] nums, int target) {
     // assume valid
     if (nums.length == 3) {
       return nums[0] + nums[1] + nums[2];
     }
+
     Arrays.sort(nums);
-    int closest = Integer.MIN_VALUE;
-    for (int i = 0; i < nums.length - 2; i++) {
-      int curClosest = twoSumClosest(nums, i + 1, nums.length - 1, nums[i], target);
-      if (curClosest == target) {
-        return target;
-      }
-      if (closest == Integer.MIN_VALUE
-          || Math.abs(curClosest - target) < Math.abs(closest - target)) {
-        closest = curClosest;
+    int n = nums.length;
+    int cloestSum = nums[0] + nums[1] + nums[2];
+    for (int k = 0; k + 2 < n; k++) {
+      int sum = twoSumCloest(nums, k + 1, n - 1, k, target);
+      if (Math.abs(sum - target) < Math.abs(cloestSum - target)) {
+        cloestSum = sum;
       }
     }
-    return closest;
+    return cloestSum;
   }
 
-  private int twoSumClosest(int[] nums, int l, int r, int init, int target) {
-    int closest = Integer.MIN_VALUE;
-    while (l < r) {
-      int sum = init + nums[l] + nums[r];
-      if (closest == Integer.MIN_VALUE
-          || Math.abs(sum - target) < Math.abs(closest - target)) {
-        closest = sum;
-      }
-      if (sum > target) {
-        r--;
-      } else if (sum < target) {
-        l++;
+  private int twoSumCloest(int[] nums, int i, int j,
+                           int k, int target) {
+    assert j - i >= 1;
+    int cloestSum = nums[k] + nums[i] + nums[j];
+    while (i < j) {
+      int sum = nums[k] + nums[i] + nums[j];
+      if (sum == target) {
+        return sum;
+      } else if (sum > target) {
+        j--;
       } else {
-        return target;
+        i++;
+      }
+      if (Math.abs(sum - target) < Math.abs(cloestSum - target)) {
+        cloestSum = sum;
       }
     }
-    return closest;
+    return cloestSum;
   }
 }
