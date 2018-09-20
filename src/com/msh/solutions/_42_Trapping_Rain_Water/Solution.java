e32a5134d646abe45341268232dda33e5a3e0597
monkeysayhi <monkeysayhi@163.com>
2018-09-20 15:42:02 +0800

    Modify

--- src/com/msh/solutions/_42_Trapping_Rain_Water/Solution.java
+++ src/com/msh/solutions/_42_Trapping_Rain_Water/Solution.java
@@ -4,33 +4,40 @@ package com.msh.solutions._42_Trapping_Rain_Water;
  * Created by monkeysayhi on 2018/4/3.
  */
 public class Solution {
-  // 截止到每个目标的盛水量仅与“该木板左侧的最高木板”（O(1)）或“该木板左侧不低于该木板的最近一个木板”（O(n)）相关 + 短板原理，总时间O(n^2)
+  // 截止到每个木板的最大容量，只取决于“该木板左侧的第一个不低于的模板”和“该木板左侧的低于的最高模板”，注意优先顺序
   public int trap(int[] height) {
     if (height == null || height.length <= 1) {
       return 0;
     }
 
-    int n = height.length;
-    int[] volumes = new int[n];
-    volumes[0] = 0;
+    int[] hs = height;
+    int n = hs.length;
+    int[] vs = new int[n];
     for (int i = 1; i < n; i++) {
-      int lastHigherIdx = -1;
-      int lastHighestIdxLtI = -1;
+      int higher = -1;
+      int lowerHighest = -1;
       for (int j = i - 1; j >= 0; j--) {
-        if (height[j] >= height[i] && lastHigherIdx == -1) {
-          lastHigherIdx = j;
-          // test case 给的不好，这里不break会TLE
+        if (hs[j] >= hs[i]) {
+          higher = j;
           break;
-        } else if (height[j] < height[i] && (lastHighestIdxLtI == -1 || height[j] > height[lastHighestIdxLtI])) {
-          lastHighestIdxLtI = j;
+        } else if (lowerHighest == -1 || hs[j] > hs[lowerHighest]) {
+          lowerHighest = j;
         }
       }
-      int lastIdx = lastHigherIdx == -1 ? lastHighestIdxLtI : lastHigherIdx;
-      volumes[i] = volumes[lastIdx] + (i - lastIdx - 1) * Math.min(height[lastIdx], height[i]);
-      for (int j = lastIdx + 1; j < i; j++) {
-        volumes[i] -= height[j];
+
+      int left = higher >= 0 ? higher : lowerHighest;
+      int h = Math.min(hs[left], hs[i]);
+      for (int j = i - 1; j > left; j--) {
+        vs[j] = h - hs[j];
       }
+      // 也可以定义 dp[i] 为“截止到位置i的最大容量和”，则 dp[i] = dp[left] + v(left, i)，答案 dp[n - 1]
     }
-    return volumes[n - 1];
+
+    int sumV = 0;
+    for (int i = 0; i < n; i++) {
+      sumV += vs[i];
+    }
+
+    return sumV;
   }
 }
