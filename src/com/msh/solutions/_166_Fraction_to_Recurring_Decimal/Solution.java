4f4b781b6d1202f342133bcc75886890b59b2e18
monkeysayhi <monkeysayhi@163.com>
2018-10-14 10:04:59 +0800

    Modify

--- src/com/msh/solutions/_166_Fraction_to_Recurring_Decimal/Solution.java
+++ src/com/msh/solutions/_166_Fraction_to_Recurring_Decimal/Solution.java
@@ -7,46 +7,47 @@ import java.util.Map;
  * Created by monkeysayhi on 2018/2/2.
  */
 public class Solution {
+  // 模拟
   // 需要好好理解无限循环小数与有限循环小数的区别。二者的小数部分都存在循环，**本质在于余数**，无限循环小数的余数是循环的
   // 分为整数与小数部分；小数部分需要统计余数的频率，如果发下余数重复出现，则为无限循环小数，循环节从该余数第一次出现的位置开始
   public String fractionToDecimal(int numerator, int denominator) {
     assert denominator != 0;
-    if (numerator == 0) {
+    boolean positive = numerator > 0 && denominator > 0
+        || numerator < 0 && denominator < 0;
+    long d1 = Math.abs((long) numerator);
+    long d2 = Math.abs((long) denominator);
+    if (d1 == 0) {
       return "0";
     }
-
-    // 标答很烂，考虑[-1, -2147483648]却没考虑[-2147483648, -1]
-    long numeratorL = numerator;
-    long denominatorL = denominator;
+    if (d1 % d2 == 0) {
+      long rs = d1 / d2;
+      return String.valueOf(positive ? rs : -rs);
+    }
 
     StringBuilder sb = new StringBuilder();
-    if ((numeratorL / Math.abs(numeratorL)) * (denominatorL / Math.abs(denominatorL)) < 0) {
-      sb.append('-');
+    if (!positive) {
+      sb.append("-");
     }
-    numeratorL = Math.abs(numeratorL);
-    denominatorL = Math.abs(denominatorL);
-
-    long d1 = numeratorL / denominatorL;
-    long d2 = numeratorL % denominatorL;
-    sb.append(d1);
-    if (d2 == 0) {
-      return sb.toString();
-    }
-
-    sb.append('.');
+    long decimalPart = d1 / d2;
+    sb.append(decimalPart);
+    sb.append(".");
+    d1 = d1 - decimalPart * d2;
+    d1 *= 10;
+    Map<Long, Integer> d1s = new HashMap<>();
     int pos = sb.length();
-    Map<Long, Integer> d2Stat = new HashMap<>();
-    while (d2 > 0) {
-      if (d2Stat.containsKey(d2)) {
-        sb.insert(d2Stat.get(d2), "(").append(")");
+    while (d1 > 0) {
+      if (d1s.containsKey(d1)) {
+        int recurStart = d1s.get(d1);
+        sb.insert(recurStart, '(');
+        sb.append(')');
         break;
       }
-      d2Stat.put(d2, pos);
-      numeratorL = d2 * 10;
-      d1 = numeratorL / denominatorL;
-      d2 = numeratorL % denominatorL;
-      assert d1 >= 0 && d1 <= 9;
-      sb.append(d1);
+      d1s.put(d1, pos);
+      long cur = d1 / d2;
+      assert cur >= 0 && cur <= 9;
+      sb.append(cur);
+      d1 = d1 - cur * d2;
+      d1 *= 10;
       pos++;
     }
     return sb.toString();
